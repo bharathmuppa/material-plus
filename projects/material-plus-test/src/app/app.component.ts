166a470e51b758a168ee6ab815b2552eb067f6a4
bharathmuppa <bharathmuppa@users.noreply.github.com>
2023-10-04 14:29:44 +0000

    Intial project setup with standalone components

--- projects/material-plus-test/src/app/app.component.ts
+++ projects/material-plus-test/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
