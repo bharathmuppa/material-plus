1a10fa32a5fe3e57d0d24cb5dcac50a6f53a4911
bharathmuppa <bharathmuppa@users.noreply.github.com>
2023-10-04 15:06:00 +0000

    Added story book and auto completed components

---
+++ projects/ngnomads/material-plus/src/lib/auto-complete/auto-complete.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone:true,
+  selector: 'lib-auto-complete',
+  templateUrl: './auto-complete.component.html',
+  styleUrls: ['./auto-complete.component.css']
+})
+export class AutoCompleteComponent {
+
+}
